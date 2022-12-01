975c9d14a014d97103fbd6e37c8a3a250701a304
kamranss <kamranss@code.edu.az>
2022-12-01 23:01:53 +0400

    Feature: Created new Project

---
+++ Practise2/Service/Mainfields.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise2.Service
+{
+    internal class Mainfields
+    {
+        public string Location;
+        public string Terminal;
+        public string VesselName;
+    }
+}
