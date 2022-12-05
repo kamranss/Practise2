11a973e4a2775fa9fa432f5c03bd9ec7d4ecf95f
kamranss <kamranss@code.edu.az>
2022-12-05 22:37:48 +0400

    Refactor: Added Array.Resize, Empty Array

---
+++ Service2/Mystr.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service2
+{
+    internal class Mystr
+    {
+        public string[] Arr;
+
+        public Mystr()
+        {
+            Arr = new string[0];
+        }
+
+        public void Add(string element)
+        {
+            Array.Resize(ref Arr,Arr.Length+1);
+        }
+    }
+}
